9a07f99504ccf5d06f24514a4d86a89d62010a27
emilychin1130 <emilychin1130@gmail.com>
2018-01-12 08:18:10 -0800

    added everything

---
+++ HealthyHarvest/HealthyHarvest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthyHarvest
+//
+//  Created by Emily Chin on 1/8/18.
+//  Copyright © 2018 Emily Chin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
